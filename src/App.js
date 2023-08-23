a043b927c0f0f6038619558563fdb99854edf1cf
Belen Caporale <caporalebelen14@gmail.com>
2023-08-23 11:19:33 -0300

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavBar from './navbar/navbar';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Routes> */}
+      <NavBar />
+
+      <Routes>
         {/* <Route path='/' element={< Home />} /> */}
+         
         cai ventures
         
-      {/* </Routes> */}
+      </Routes>
     </BrowserRouter>
     
   );
