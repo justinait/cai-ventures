ba46ac4a256d869d1c1726b68fcddc53beb05bde
Layenx Laventure <llayenx@gmail.com>
2023-08-23 10:27:48 -0300

    first commit services

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import services from "../services"
+
+const Services = () => {
+    return (
+        <div>
+            <h1>Hola Services</h1>
+        </div>
+    );
+};
+
+export default Services;
