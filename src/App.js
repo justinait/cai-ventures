00c8f01ee7a1a42822908d7b80fb0d75f394c238
Eduardo Tolaba <eduardoetolaba@gmail.com>
2023-08-23 11:47:17 -0300

    create components footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         CAI VENTURES
         
       </Routes>
+      <Footer/>
     </BrowserRouter>
     
   );
